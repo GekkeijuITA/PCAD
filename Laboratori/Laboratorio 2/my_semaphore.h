2d8b63bcc3892319a90bd69168cec57b61f336c6
Gekkeiju <gekkeijuita@gmail.com>
2025-04-09 11:52:13 +0200

    Labo 2

---
+++ Laboratori/Laboratorio 2/my_semaphore.h
@@ -0,0 +1,18 @@
+#ifndef MY_SEMAPHORE_H
+#define MY_SEMAPHORE_H
+
+#include <pthread.h>
+
+typedef struct my_semaphore
+{
+    volatile unsigned int V;
+    pthread_mutex_t lock;
+    pthread_cond_t varcond;
+} my_semaphore;
+
+int my_sem_init(my_semaphore *ms, unsigned int v);
+int my_sem_wait(my_semaphore *ms);
+int my_sem_signal(my_semaphore *ms);
+int my_sem_destroy(my_semaphore *ms);
+
+#endif // MY_SEMAPHORE_H
